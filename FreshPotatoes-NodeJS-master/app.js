3da974ae121ab966663f55139b2a83d8eee9b21d
jasminejeane <jasminejeane@gmail.com>
2018-01-12 13:49:15 -0800

    created router.js , set up app.js to req files and packages, set up port

--- FreshPotatoes-NodeJS-master/app.js
+++ FreshPotatoes-NodeJS-master/app.js
@@ -1,4 +1,18 @@
-var request = require("request");
+'use strict';
+
+var express = require("express"),
+    app = express(),
+    routes = require("./routes"),
+    request = require("request"),
+
+    jsonParser = require("body-parser").json;
+
+
+
+
+
+
+
 
 request("https://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=8", function(error, response, body) {
 
@@ -6,6 +20,12 @@ request("https://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe91
   if (!error && response.statusCode === 200) {
 
     // Parse the body of the site
-    console.log("movie: " + body);
+    console.log("movie: " + JSON.parse(body));
   }
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+	console.log("Express server is listening on port", port);
 });
