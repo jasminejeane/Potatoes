af22407f2681c63f32b185892865ac2329ff5a16
jasminejeane <jasminejeane@gmail.com>
2018-01-12 14:45:22 -0800

    connected to the sql db

--- FreshPotatoes-NodeJS-master/app.js
+++ FreshPotatoes-NodeJS-master/app.js
@@ -2,6 +2,7 @@
 
 var express = require("express"),
     app = express(),
+    sqlite3 = require('sqlite3').verbose(),
     routes = require("./routes"),
     request = require("request"),
 
@@ -15,6 +16,34 @@ var express = require("express"),
 
     // status codes for api responses
     app.use(jsonParser());
+
+
+    var db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READONLY, (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log('Connected to the sqlite database.');
+    });
+
+    // db.serialize(() => {
+    //   db.each(`SELECT PlaylistId as id,
+    //                   Name as name
+    //            FROM playlists`, (err, row) => {
+    //     if (err) {
+    //       console.error(err.message);
+    //     }
+    //     console.log(row.id + "\t" + row.name);
+    //   });
+    // });
+
+    db.close((err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log('Close the database connection.');
+    });
+
+
 
     app.use("/", routes);
 
