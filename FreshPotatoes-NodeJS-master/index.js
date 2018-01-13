5dcd615ebb137fc373e3bef139f14e1c2b65e278
jasminejeane <jasminejeane@gmail.com>
2018-01-13 07:29:06 -0800

    helper thirdparty function recieves req from nesting in getFilmRecommendations func

--- FreshPotatoes-NodeJS-master/index.js
+++ FreshPotatoes-NodeJS-master/index.js
@@ -88,45 +88,7 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 function getFilmRecommendations(req, res) {
 
   thirdPartyReq(req.params.id);
-  // console.log("body", req.params.id);
-
-
-  // request("https://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=10206",
-  //   function(error, response, body) {
-  //
-  // if(error){
-  // console.error(error);
-  // }
-  //
-  // var reviews,
-  // average = 0,
-  // newAverage;
-  //     // If the request is successful (i.e. if the response status code is 200)
-  //     if (!error && response.statusCode === 200) {
-  //
-  //       console.log(body);
-  //       reviews = JSON.parse(body)[0].reviews;
-  //       // criteria -  A minimum of 5 reviews
-  //       if (reviews.length >= 5) {
-  //
-  //         for (var i = 0; i < reviews.length; i++) {
-  //
-  //           average += reviews[i].rating;
-  //         }
-  //         newAverage = average / reviews.length;
-  //         console.log("average before if greater: ", newAverage.toFixed(1));
-  //
-  //       }
-  //
-  //       // criteria -  greater than 4.0
-  //       // criteria -  number of reviews
-  //       if (newAverage > 4.0) {
-  //         console.log("average", newAverage.toFixed(1));
-  //         console.log("number of reviews", reviews.length)
-  //
-  //       }
-  //     }
-  //   })
+  console.log("newAverage", newAverage);
 
   let sql = `SELECT films.id, films.title, films.release_date,
             genres.name from films LEFT JOIN genres on
