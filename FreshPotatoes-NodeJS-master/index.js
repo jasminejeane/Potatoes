bc5fbad56638f5dc20943972e8f51b1e39419374
jasminejeane <jasminejeane@gmail.com>
2018-01-15 20:55:14 -0800

    10 passing test

--- FreshPotatoes-NodeJS-master/index.js
+++ FreshPotatoes-NodeJS-master/index.js
@@ -18,48 +18,48 @@ Promise.resolve()
   });
 
 
-function thirdPartyReq(id) {
-  var url = "https://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=" + id;
-  // console.log("id", id);
-  // console.log("url", url);
-
-  request(url,
-    function(error, response, body) {
-
-      if (error) {
-        console.error(error);
-      }
-
-      var reviews,
-        average = 0,
-        newAverage;
-      // If the request is successful (i.e. if the response status code is 200)
-      if (!error && response.statusCode === 200) {
-
-        // console.log(body);
-        reviews = JSON.parse(body)[0].reviews;
-        // criteria -  A minimum of 5 reviews
-        if (reviews.length >= 5) {
-
-          for (var i = 0; i < reviews.length; i++) {
-
-            average += reviews[i].rating;
-          }
-          newAverage = average / reviews.length;
-          console.log("average before if greater: ", newAverage.toFixed(1));
-
-        }
-
-        // criteria -  greater than 4.0
-        // criteria -  number of reviews
-        if (newAverage > 4.0) {
-          console.log("average", newAverage.toFixed(1));
-          console.log("number of reviews", reviews.length)
-
-        }
-      }
-    })
-}
+// function thirdPartyReq(id) {
+//   var url = "https://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=" + id;
+//   // console.log("id", id);
+//   // console.log("url", url);
+//
+//   request(url,
+//     function(error, response, body) {
+//
+//       if (error) {
+//         console.error(error);
+//       }
+//
+//       var reviews,
+//         average = 0,
+//         newAverage;
+//       // If the request is successful (i.e. if the response status code is 200)
+//       if (!error && response.statusCode === 200) {
+//
+//         // console.log(body);
+//         reviews = JSON.parse(body)[0].reviews;
+//         // criteria -  A minimum of 5 reviews
+//         if (reviews.length >= 5) {
+//
+//           for (var i = 0; i < reviews.length; i++) {
+//
+//             average += reviews[i].rating;
+//           }
+//           newAverage = average / reviews.length;
+//           console.log("average before if greater: ", newAverage.toFixed(1));
+//
+//         }
+//
+//         // criteria -  greater than 4.0
+//         // criteria -  number of reviews
+//         if (newAverage > 4.0) {
+//           console.log("average", newAverage.toFixed(1));
+//           console.log("number of reviews", reviews.length)
+//
+//         }
+//       }
+//     })
+// }
 
 
 // THIRD PARTY CONNECTION
@@ -80,11 +80,28 @@ var db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READONLY, (err) =
 
 
 // ROUTES
-app.get('/films/:id/recommendations', getFilmRecommendations);
+app.get('/films/:id/recommendations',
+function(req, res, next){
+
+  // req.param('name')
+		if(isNaN(req.params.id)) {
+			var err = new Error("Not Found");
+			err.status = 422;
+      console.log("NAN");
+
+      // res.json({
+    	// 		message: 422
+    	// });
+console.log("param id", req.params.id);
+			next(err);
+		} else {
+			next();
+		}
+	},
+   getFilmRecommendations);
 
 
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next){
 	var err = new Error("Not Found");
 	err.status = 404;
@@ -92,14 +109,14 @@ app.use(function(req, res, next){
 });
 
 // Error Handler
-
+// handles missing routes
 app.use(function(err, req, res, next){
 	res.status(err.status || 500);
-	res.json({
-		// error: {
-			message: err.message
-		// }
-	});
+
+  res.json({
+      message: err.message
+  });
+
 });
 // express middleware optional params
 // or another route for the limit and offset
@@ -125,7 +142,7 @@ function getFilmRecommendations(req, res) {
 
     var offset = parseInt(req.query.offset, 10);
     if (isNaN(offset)) {
-      res.status(422).send("Unprocessable Entity, got 400 Bad Request");
+      // res.status(422).send("Unprocessable Entity, got 400 Bad Request");
 
     }else if (offset < 1) {
       offset = 0;
@@ -152,11 +169,7 @@ function getFilmRecommendations(req, res) {
 
     db.all(sql + req.params.id + ") LIMIT " + limit , [], (err, rows) => {
       // process rows here
-      if (err) {
-        res.status(400).send( {"message" : 404});
 
-        res.status(500).send('Not Implemented');
-      }
       // console.log("req Param works", JSON.stringify(rows, null, 2));
       // resolve(rows);
       res.json({
@@ -210,12 +223,6 @@ function getFilmRecommendations(req, res) {
 
 };
 
-// res.status(200).json({
-//
-//   recommendations: "You sent me a GET request - 200",
-//   response: rows,
-//
-//
-// })
+
 // https://gist.github.com/dalelane/6ce08b52d5cca8f92926
 module.exports = app;
