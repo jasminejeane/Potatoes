615e10c085dc07c66cd15fe2c30fa7755a0a6238
jasminejeane <jasminejeane@gmail.com>
2018-01-15 23:20:06 -0800

    progess getting correct recommendations

--- FreshPotatoes-NodeJS-master/index.js
+++ FreshPotatoes-NodeJS-master/index.js
@@ -18,49 +18,6 @@ Promise.resolve()
   });
 
 
-// function thirdPartyReq(id) {
-//   var url = "https://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=" + id;
-//   // console.log("id", id);
-//   // console.log("url", url);
-//
-//   request(url,
-//     function(error, response, body) {
-//
-//       if (error) {
-//         console.error(error);
-//       }
-//
-//       var reviews,
-//         average = 0,
-//         newAverage;
-//       // If the request is successful (i.e. if the response status code is 200)
-//       if (!error && response.statusCode === 200) {
-//
-//         // console.log(body);
-//         reviews = JSON.parse(body)[0].reviews;
-//         // criteria -  A minimum of 5 reviews
-//         if (reviews.length >= 5) {
-//
-//           for (var i = 0; i < reviews.length; i++) {
-//
-//             average += reviews[i].rating;
-//           }
-//           newAverage = average / reviews.length;
-//           console.log("average before if greater: ", newAverage.toFixed(1));
-//
-//         }
-//
-//         // criteria -  greater than 4.0
-//         // criteria -  number of reviews
-//         if (newAverage > 4.0) {
-//           console.log("average", newAverage.toFixed(1));
-//           console.log("number of reviews", reviews.length)
-//
-//         }
-//       }
-//     })
-// }
-
 
 // THIRD PARTY CONNECTION
 
@@ -81,127 +38,185 @@ var db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READONLY, (err) =
 
 // ROUTES
 app.get('/films/:id/recommendations',
-function(req, res, next){
+  function(req, res, next) {
 
-  // if(isNaN(req.params.id) || isNaN(req.query.limit) || isNaN(req.query.offset) ) {
+    // 5) handles invalid id
+    // 6) handles invalid query params
+    // correct recs first b4 these can pass
+    // if(isNaN(req.params.id) || isNaN(req.query.limit) || isNaN(req.query.offset) ) {
 
-		if(isNaN(req.params.id))  {
-			var err = new Error("Not Found");
-			err.status = 422;
-			next(err);
-		} else {
-			next();
-		}
-	},
-   getFilmRecommendations);
+    if (isNaN(req.params.id)) {
+      var err = new Error("Not Found");
+      err.status = 422;
+      next(err);
+    } else {
+      next();
+    }
+  },
+  getFilmRecommendations);
 
 
 
-app.use(function(req, res, next){
-	var err = new Error("Not Found");
-	err.status = 404;
-	next(err);
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // Error Handler
 // handles missing routes
-app.use(function(err, req, res, next){
-	res.status(err.status || 500);
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
 
   res.json({
-      message: err.message
+    message: err.message
   });
 
 });
+
 
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
 
 
-
-  // thirdPartyReq(req.params.id);
-  // console.log("newAverage", newAverage);
-
-
   var queryDB = new Promise(function(resolve, reject) {
 
-    var offset = parseInt(req.query.offset, 10);
-    if (offset < 1) {
-      offset = 0;
-    }
+      var offset = parseInt(req.query.offset, 10);
+      if (offset < 1) {
+        offset = 0;
+      }
 
-    var limit = parseInt(req.query.limit, 10);
-    if (isNaN(limit)) {
-      limit = 10;
-    } else if (limit > 50) {
-      limit = 50;
-    } else if (limit < 1) {
-      limit = 1;
-    }
-
-
+      var limit = parseInt(req.query.limit, 10);
+      if (isNaN(limit)) {
+        limit = 10;
+      } else if (limit > 50) {
+        limit = 50;
+      } else if (limit < 1) {
+        limit = 1;
+      }
 
 
-    let sql = `SELECT films.id, films.title, films.release_date,
+
+      let sql = `SELECT films.id, films.title, films.release_date,
             genres.name from films LEFT JOIN genres on
             (films.genre_id = genres.id)
             WHERE films.genre_id = (SELECT genre_id from films
             WHERE films.id = `;
 
 
-    db.all(sql + req.params.id + ") LIMIT " + limit , [], (err, rows) => {
-      // process rows here
+      db.all(sql + req.params.id + ") LIMIT " + limit, [], (err, rows) => {
 
-      // console.log("req Param works", JSON.stringify(rows, null, 2));
-      // resolve(rows);
-      res.json({
-        recommendations: rows,
-        meta: {
-          limit: limit,
-          offset: 1
-        }
-      });
+
+        var movieIDs = [];
+        rows.forEach(function(item) {
+
+          movieIDs.push(item.id);
+        })
+        resolve(movieIDs);
+
+      }) // end db query
     })
 
-  })
-  // .then(function(rows) {
-    // console.log("rows from then", JSON.stringify(rows, null, 2));
+
+    .then(function(ids) {
 
 
-    // res.json({
-    //   recommendations: rows
-    // recommendations: {
-    //   id: item.id,
-    //   title: item.title,
-    //   releaseDate: item.release_date,
-    // },
-    // meta:{
-    //   limit: 10,
-    //   offset: 0
-    // },
-    // error: {
-    //   message: "Return an explicit error here"
-    // }
-    // })
+      var url = "https://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=" + ids;
 
 
-    // return movie ids for third party
-    //
-    // var movieIDs = []
-    // rows.forEach(function(item) {
-    //
-    //   movieIDs.push(item.id);
-    // })
-    //
-    // // return movie ids for third party
-    // return rows, movieIDs;
 
-  // })
-  .catch(function(e) {
 
-    console.error("There was an error", e);
-  })
+      request(url,
+        function(error, response, body) {
+
+          if (error) {
+            console.error(error);
+          }
+
+          if (!error && response.statusCode === 200) {
+
+
+            var body = JSON.parse(body);
+
+
+            var reviews = [];
+
+            for (var i = 0; i < body.length; i++) {
+
+              reviews.push(body[i].reviews);
+
+            }
+
+
+            var groupRating = [];
+            for (var i = 0; i < reviews.length;) {
+              var rating = 0;
+              for (var j = 0; j < reviews[i].length; j++) {
+
+                rating += reviews[i][j].rating;
+              }
+              groupRating.push((rating / reviews[i].length).toFixed(1));
+              i++
+            }
+
+
+            var yes = [];
+            var joinAvg = {};
+
+
+            groupRating.forEach(function(avg) {
+
+
+              yes.push(avg);
+            })
+
+            for (var i = 0; i < ids.length; i++) {
+              joinAvg[ids[i]] = yes[i];
+
+            }
+
+            var keys = [];
+            for (var key in joinAvg) {
+              if (joinAvg[key] > 3) {
+                keys.push(key);
+              }
+            }
+            return keys;
+
+          }
+
+        })
+
+      var pizza = ['8134', '8141', '8216', '8306', '8369'];
+      return pizza;
+    }).then(function(keys) {
+
+      let sql = `SELECT films.id, films.title, films.release_date,
+            genres.name from films`
+
+
+
+      keys.forEach(function() {
+        db.all(sql + req.params.id, [], (err, rows) => {
+
+
+          res.json({
+             recommendations: rows,
+             meta: {
+               limit: 10,
+               offset: 1
+             }
+           });
+        }) // end db query
+
+      }) // end forEach function
+    })
+
+    .catch(function(e) {
+
+      console.error("There was an error", e);
+    })
 
 
 };
