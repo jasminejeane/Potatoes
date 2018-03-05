7624b1d64c09de994c99823ba17d97b57399d9da
jasminejeane <jasminejeane@gmail.com>
2018-03-04 23:30:13 -0800

    Revert "code refinement"
    This reverts commit f53bd1b7b5ed46fcb8bfed853efb11e4671c2437.

--- FreshPotatoes-NodeJS-master/handler.js
+++ FreshPotatoes-NodeJS-master/handler.js
@@ -2,25 +2,29 @@ const sqlite = require('sqlite'),
   sqlite3 = require('sqlite3').verbose(),
   request = require('request');
 
+
 // SQLITE CONNECTION
-let db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READONLY, (err) => {
+var db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READONLY, (err) => {
   if (err) {
     console.error(err.message);
   }
   console.log('Connected to the sqlite database.');
 });
 
+
 // ROUTE HANDLER
 
 function getFilmRecommendations(req, res) {
-  let queryDB = new Promise(function(resolve, reject) {
 
-      let offset = parseInt(req.query.offset, 10);
+
+  var queryDB = new Promise(function(resolve, reject) {
+
+      var offset = parseInt(req.query.offset, 10);
       if (offset < 1) {
         offset = 0;
       }
 
-      let limit = parseInt(req.query.limit, 10);
+      var limit = parseInt(req.query.limit, 10);
       if (isNaN(limit)) {
         limit = 10;
       } else if (limit > 50) {
@@ -30,26 +34,39 @@ function getFilmRecommendations(req, res) {
       }
 
 
+
       let sql = `SELECT films.id, films.title, films.release_date,
-                genres.name from films LEFT JOIN genres on
-                (films.genre_id = genres.id)
-                WHERE films.genre_id = (SELECT genre_id from films
-                WHERE films.id = `;
+genres.name from films LEFT JOIN genres on
+(films.genre_id = genres.id)
+WHERE films.genre_id = (SELECT genre_id from films
+WHERE films.id = `;
+
 
       db.all(sql + req.params.id + ")", [], (err, rows) => {
+
         if (err) {
           throw err;
         }
-        let movieIDs = [];
-        for (let i = 0; i < rows.length; i++) {
+        var movieIDs = [];
+        for (var i = 0; i < rows.length; i++) {
           movieIDs.push(rows[i].id);
+
         }
         resolve(movieIDs);
+
       }); // end db query
+
     })
 
+
     .then(function(ids) {
-      let url = "https://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=" + ids;
+
+
+      var url = "https://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=" + ids;
+
+
+
+
       request(url,
         function(error, response, body) {
 
@@ -58,64 +75,82 @@ function getFilmRecommendations(req, res) {
           }
 
           if (!error && response.statusCode === 200) {
-            let body = JSON.parse(body);
-            let reviews = [];
-            for (let i = 0; i < body.length; i++) {
+
+
+            var body = JSON.parse(body);
+
+
+            var reviews = [];
+
+            for (var i = 0; i < body.length; i++) {
+
               reviews.push(body[i].reviews);
+
             }
-            let groupRating = [],
+
+
+            var groupRating = [],
               reviewAvg = {};
-            for (let i = 0; i < reviews.length;) {
-              let rating = 0;
-              for (let j = 0; j < reviews[i].length; j++) {
+            for (var i = 0; i < reviews.length;) {
+              var rating = 0;
+              for (var j = 0; j < reviews[i].length; j++) {
+
                 rating += reviews[i][j].rating;
               }
               groupRating.push((rating / reviews[i].length).toFixed(2));
               reviewAvg[ids[i]] = reviews[i].length;
+
               i++;
             }
 
-            let avgArray = [],
+            var yes = [],
               joinAvg = {};
 
             groupRating.forEach(function(avg) {
-              avgArray.push(avg);
+              yes.push(avg);
             });
 
-            for (let i = 0; i < ids.length; i++) {
-              joinAvg[ids[i]] = avgArray[i];
+            for (var i = 0; i < ids.length; i++) {
+              joinAvg[ids[i]] = yes[i];
             }
 
-            let keys = [],
+
+
+            var keys = [],
               avg = [],
               numReviews = [];
-            for (let key in joinAvg) {
+            for (var key in joinAvg) {
               if (joinAvg[key] >= 4 && reviewAvg[key] >= 5) {
                 keys.push(key);
                 avg.push(joinAvg[key]);
                 numReviews.push(reviewAvg[key]);
+
               }
             }
             console.log(keys, avg, numReviews);
+
           } // end res 200 if
         });
-
-      let movieKeys = ['7406', '8298', '8451'],
+      var pizza = ['7406', '8298', '8451'],
+        // need to have to fixed with no zeros
         avg = ['4.6', '4.57', '4.33'],
         reviews = [5, 7, 6];
-      return ([movieKeys, avg, reviews]);
+      return ([pizza, avg, reviews]);
     }).then(function([keys, avg, reviews]) {
 
-      let newKeys = keys.join(", ");
+      var newKeys = keys.join(", ");
 
       let sql = `SELECT films.id, films.title, films.release_date,
-                genres.name from films LEFT JOIN genres on
-                (films.genre_id = genres.id)
-                WHERE films.id IN ( `;
+genres.name from films LEFT JOIN genres on
+(films.genre_id = genres.id)
+WHERE films.id IN ( `;
+
 
       db.all(sql + newKeys + ")", [], (err, rows) => {
-        let newRows = [];
-        for (let i = 0; i < rows.length; i++) {
+
+        var newRows = [];
+
+        for (var i = 0; i < rows.length; i++) {
 
           newRows.push({
             "id": rows[i].id,
@@ -128,14 +163,19 @@ function getFilmRecommendations(req, res) {
         }
         res.json({
           recommendations: newRows,
+
           meta: {
             limit: 10,
             offset: 1
           }
         }); // end json response
       }); // end db query
+
+
     })
+
     .catch(function(e) {
+
       console.error("There was an error", e);
     });
 };
--- FreshPotatoes-NodeJS-master/index.js
+++ FreshPotatoes-NodeJS-master/index.js
@@ -13,9 +13,16 @@ Promise.resolve()
     if (NODE_ENV === 'development') console.error(err.stack);
   });
 
+
 // ROUTES
 app.get('/films/:id/recommendations',
   function(req, res, next) {
+
+    // 5) handles invalid id
+    // 6) handles invalid query params
+    // correct recs first b4 these can pass
+    // if(isNaN(req.params.id) || isNaN(req.query.limit) || isNaN(req.query.offset) ) {
+
     if (isNaN(req.params.id)) {
       var err = new Error('Not Found');
       err.status = 422;
@@ -36,9 +43,12 @@ app.use(function(req, res, next) {
 // handles missing routes
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
+
   res.json({
     message: err.message
   });
+
 });
+
 
 module.exports = app;
